adb02f602efa90c67b7fd2e174b7fe85257a454d
Michael <michael.brauweiler@gmail.com>
2018-05-21 15:46:55 +0200

    Refactor

--- src/main/java/solver/Solver.java
+++ src/main/java/solver/Solver.java
@@ -1,35 +1,24 @@
 package solver;
 
-import com.github.javaparser.JavaParser;
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.Node;
-import com.github.javaparser.ast.NodeList;
-import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.body.Parameter;
-import com.github.javaparser.ast.body.VariableDeclarator;
-import com.github.javaparser.ast.comments.Comment;
-import com.github.javaparser.ast.comments.JavadocComment;
-import com.github.javaparser.ast.expr.MethodCallExpr;
-import com.github.javaparser.ast.stmt.BlockStmt;
-import com.github.javaparser.ast.visitor.VoidVisitor;
-import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
-import com.github.javaparser.symbolsolver.javaparser.Navigator;
-import com.github.javaparser.symbolsolver.model.resolution.TypeSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
 
 import org.apache.commons.io.FileUtils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.visitor.VoidVisitor;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.opencsv.CSVWriter;
 
 public class Solver {
 
@@ -41,7 +30,7 @@ public class Solver {
     private static final String SRC_PATH = "src/main/resources/Time/";
 
     public static void main(String[] args) throws Exception {
-        for(File file : getSubfolderClasses(FILE_PATH)){
+        for (File file : getSubfolderClasses(FILE_PATH)) {
             currentFile = file;
             CompilationUnit cu = JavaParser.parse(new FileInputStream(file));
             VoidVisitor<?> methodNameVisitor = new MethodNamePrinter();
@@ -54,154 +43,85 @@ public class Solver {
         @Override
         public void visit(MethodDeclaration md, Void arg) {
             super.visit(md, arg);
+            System.out.println("==========================================");
             System.out.println("File: " + currentFile);
             System.out.println("Method Name: " + md.getName());
             Optional<BlockStmt> blk = md.getBody();
-            if (blk.isPresent()){
+            if (blk.isPresent()) {
                 BlockStmt block = blk.get();
                 String text = block.toString();
-                int words = getWordCount(text);
-                int lines = getLineCount(text);
-                int repWords = getMaxRepeatedWords(text);
-                float density = getDensity(words, lines);
-                int arguments = getArgCount(md);
-                int funcCalls = getFuncCallCount(md);
-                int variables = getLocalVarCount(md);
-                float commentRatio = getCommentRatio(md);
-                int maxDepth = getMaxDepth(md);
+                String signature = md.getSignature().asString();
+                int words = Metrics.getWordCount(text);
+                int lines = Metrics.getLineCount(text);
+                int repWords = Metrics.getMaxRepeatedWords(text);
+                float density = Metrics.getDensity(words, lines);
+                int arguments = Metrics.getParCount(md);
+                int funCalls = Metrics.getFuncCallCount(md);
+                int variables = Metrics.getLocalVarCount(md);
+                float commentRatio = Metrics.getCommentRatio(md);
+                int maxDepth = Metrics.getMaxDepth(md);
+                int blocks = Metrics.getBlockCount(md);
+
+                StaticResult result = new StaticResult(
+                    currentFile,
+                    signature,
+                    words,
+                    lines,
+                    density,
+                    arguments,
+                    funCalls,
+                    variables,
+                    repWords,
+                    commentRatio,
+                    maxDepth,
+                    blocks
+                );
 
                 System.out.println("==========================================");
                 System.out.println(md);
-                System.out.println("#Words: " + words);
-                System.out.println("#Lines: " + lines);
-                System.out.println("Density: " + density);
-                System.out.println("#Arguments: " + arguments);
-                System.out.println("#Method Calls: " + funcCalls);
-                System.out.println("#Variables: " + variables);
-                System.out.println("Maximal repeated words: " + repWords);
-                System.out.println("Comments ratio: " + commentRatio);
-                System.out.println("Maximal depth: " + maxDepth);
-                System.out.println("");
                 System.out.println("==========================================");
+
+                String[] headerRecord = result.getHeader();
+                String[] lineRecord = result.getRecord();
+                writeCSVFile(CSV_PATH, headerRecord, lineRecord);
+            } else {
+                System.out.println("No method block.");
             }
         }
     }
 
-    private static List<File> getSubfolderClasses(String path){
+    private static List<File> getSubfolderClasses(String path) {
         File rootFile = new File(path);
         List<File> files = new LinkedList<File>();
-        if(rootFile.isDirectory()){
+        if (rootFile.isDirectory()) {
             files = (List<File>) FileUtils.listFiles(rootFile, new String[] { "java" }, true);
-        } else if(rootFile.isFile()){
+        } else if (rootFile.isFile()) {
             files.add(rootFile);
-        }
-        else {
+        } else {
             System.out.println("File path not correct.");
             System.exit(1);
-            }
+        }
         return files;
     }
 
-    private static int getWordCount(String text) {
-        String cleaned = text.replaceAll("[^\\p{L}\\p{Nd}]+", " ").trim();
-        String[] words = cleaned.split(" ");
-        return words.length;
-    }
-
-    private static int getMaxRepeatedWords(String text) {
-        String cleaned = text.replaceAll("[^\\p{L}\\p{Nd}]+", " ").trim();
-        String[] words = cleaned.split(" ");
-        Map<String, Integer> occurences = new HashMap<String, Integer>();
-            for(String word : words){
-                Integer oldCount = occurences.get(word);
-                if(oldCount == null){
-                    oldCount = 0;
-                }
-                occurences.put(word, oldCount+1);
-            }
-        return Collections.max(occurences.values());
-        
-    }
-
-    private static int getLineCount(String text) {
-        String[] lines = text.split("\r\n|\r|\n");
-        return lines.length - 2;
-    }
-
-    private static float getDensity(int words, int lines) {
-        return (float) words/ lines;
-    }
-
-    private static int getArgCount(MethodDeclaration md){
-        NodeList<Parameter> parameters = md.getParameters();
-        return parameters.size();
-    }
-
-    private static int getFuncCallCount(MethodDeclaration md){
-        List<MethodCallExpr> methodCalls = Navigator.findAllNodesOfGivenClass(md, MethodCallExpr.class);
-        return methodCalls.size();
-    }
-        // TypeSolver jpTypeSolver = new JavaParserTypeSolver(SRC_PATH);
-        // TypeSolver reTypeSolver = new ReflectionTypeSolver();
-        // TypeSolver combSolver = new CombinedTypeSolver(jpTypeSolver, reTypeSolver);
-
-        // Set<String> set = new HashSet<String>();
-        // for(MethodCallExpr element : methodCalls) {
-            // set.add(element.toString()); 
-            // System.out.println(element);
-        // }
-
-
-        // methodCalls.forEach(mc -> set.add(JavaParserFacade.get(combSolver)
-                    // .solve(mc)
-                    // .getCorrespondingDeclaration()
-                    // .getQualifiedSignature()));
-
-    private static int getLocalVarCount(MethodDeclaration md){
-        List<VariableDeclarator> declarations = Navigator.findAllNodesOfGivenClass(md, VariableDeclarator.class);
-        return declarations.size();
-    }
-    
-    private static float getCommentRatio(MethodDeclaration md){
-        List<Comment> innerComments = md.getAllContainedComments();
-        Optional<JavadocComment> docComments = md.getJavadocComment();
-        String methodText = md.getBody().toString();
-        // Extract inner method comments and javadoc comments
-        String commentText = innerComments.toString();
-        if(docComments.isPresent()) commentText += docComments.get().toString();
-        // Delete everything except letters and numerals
-        String cleanedComments = commentText.replaceAll("[^\\p{L}\\p{Nd}]+", "").trim();
-        String cleanedMethod = methodText.replaceAll("[^\\p{L}\\p{Nd}]+", "").trim();
-        // Calculate percentage of comments
-        int commentChars = cleanedComments.toCharArray().length;
-        int methodChars = cleanedMethod.toCharArray().length;
-        if (commentChars > 0 || methodChars > 0) {
-            return (float) commentChars / (methodChars + commentChars);
-        }
-        return 0;
-    }
-
-    private static int getMaxDepth(MethodDeclaration md){
-        List<Node> frontier = new LinkedList<Node>();
-        frontier.add(md);
-        int depth = 0;
-        while(!frontier.isEmpty()){
-            List<Node> children = new LinkedList<Node>();
-            for(Node node : frontier){
-                children.addAll(node.getChildNodesByType(BlockStmt.class));
-            }
-            frontier.clear();
-            if(!children.isEmpty()){
-                frontier.addAll(children);
-                depth++;
+    private static void writeCSVFile(String path, String[] header, String[] args) {
+        File file = new File(path);
+        if (!file.exists()) {
+            try (
+                    Writer writer = Files.newBufferedWriter(Paths.get(path));
+                    CSVWriter csvWriter = new CSVWriter(writer,
+                                                        CSVWriter.DEFAULT_SEPARATOR,
+                                                        CSVWriter.NO_QUOTE_CHARACTER,
+                                                        CSVWriter.DEFAULT_ESCAPE_CHARACTER,
+                                                        CSVWriter.DEFAULT_LINE_END); ) {
+                csvWriter.writeNext(header);
+                csvWriter.writeNext(args);
+            } catch (IOException e) {
+                System.err.println("Error during CSV file generation! Aborting ...");
             }
         }
-        return depth;
+        else{
+// TODO appending data:  <20-05-18, yourname> //
+        }
     }
-
-    private static int getBlockCount(MethodDeclaration md){
-        List<BlockStmt> blocks = Navigator.findAllNodesOfGivenClass(md, BlockStmt.class);
-        return blocks.size();
-    }
-
 }
---
+++ src/main/java/solver/StaticResult.java
@@ -0,0 +1,113 @@
+package solver;
+
+import java.io.File;
+
+import org.apache.commons.lang3.Validate;
+
+public class StaticResult {
+
+    private File file;
+    private String signature;
+    private int words;
+    private int lines;
+    private float density;
+    private int arguments;
+    private int methodCalls;
+    private int variables;
+    private int repWords;
+    private float commentRatio;
+    private int maxDepth;
+    private int blocks;
+
+    public StaticResult(
+        File file,
+        String signature,
+        int words,
+        int lines,
+        float density,
+        int arguments,
+        int methodCalls,
+        int variables,
+        int repWords,
+        float commentRatio,
+        int maxDepth,
+        int blocks
+    ) {
+        this.file = file;
+        this.signature = signature;
+        this.words = words;
+        this.lines = lines;
+        this.density = density;
+        this.arguments = arguments;
+        this.methodCalls = methodCalls;
+        this.variables = variables;
+        this.repWords = repWords;
+        this.commentRatio = commentRatio;
+        this.maxDepth = maxDepth;
+        this.blocks = blocks;
+
+        Validate.notNull(file, "File can not be null.");
+        Validate.notNull(signature, "Signature can not be null.");
+        Validate.notNull(words, "Words can not be null.");
+        Validate.notNull(lines, "Lines can not be null.");
+        Validate.notNull(density, "Density can not be null.");
+        Validate.notNull(arguments, "Arguments can not be null.");
+        Validate.notNull(methodCalls, "MethodCalls can not be null.");
+        Validate.notNull(variables, "Variables can not be null.");
+        Validate.notNull(repWords, "RepWords can not be null.");
+        Validate.notNull(commentRatio, "CommentRatio can not be null.");
+        Validate.notNull(maxDepth, "MaxDepth can not be null.");
+        Validate.notNull(blocks, "Blocks can not be null.");
+    }
+
+    public String[] getRecord() {
+        String[] record = {
+            "" + this.words,
+            "" + this.lines,
+            "" + this.density,
+            "" + this.arguments,
+            "" + this.methodCalls,
+            "" + this.variables,
+            "" + this.repWords,
+            "" + this.commentRatio,
+            "" + this.maxDepth,
+            "" + this.blocks
+        };
+        return record;
+    }
+
+    public String[] getHeader() {
+        String[] headerRecord = {
+            "FilePath",
+            "MethodName",
+            "Words",
+            "Lines",
+            "Density",
+            "Arguments",
+            "MethodCalls",
+            "Variables",
+            "MaxRepWords",
+            "CommentRatio",
+            "MaxDepth",
+            "Blocks"
+        };
+        return headerRecord;
+    }
+
+    public String toString() {
+        String text = "";
+        text += "File::signature: " +
+                this.file.getPath() + "::" + this.signature + "\n";
+        text += "Number of words: " + this.words + "\n";
+        text += "Number of lines: " + this.lines + "\n";
+        text += "Density: " + this.density + "\n";
+        text += "Number of arguments: " + this.arguments + "\n";
+        text += "Number of method calls: " + this.methodCalls + "\n";
+        text += "Number of variables: " + this.variables + "\n";
+        text += "Maximal of repeated words: " + this.repWords + "\n";
+        text += "Comment ratio: " + this.commentRatio + "\n";
+        text += "Maximum depth: " + this.maxDepth + "\n";
+        text += "Number of blocks: " + this.blocks + "\n";
+        return text;
+    }
+}
